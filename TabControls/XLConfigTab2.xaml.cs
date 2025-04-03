1417d6561fc6b77f6c9fddd49eee08c5803a83b0
Domin <dominiknowekonto@gmail.com>
2025-04-03 19:54:36 +0200

    create repo

---
+++ TabControls/XLConfigTab2.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using XLPilot.UserControls;
+
+namespace XLPilot.TabControls
+{
+    /// <summary>
+    /// Interaction logic for XLConfigTab2.xaml
+    /// </summary>
+    public partial class XLConfigTab2 : UserControl
+    {
+        public XLConfigTab2()
+        {
+            InitializeComponent();
+        }
+
+        // If you need these properties in this class, register them with XLConfigTab2 as the owner
+    //    public string ImageSource
+    //    {
+    //        get => (string)GetValue(ImageSourceProperty);
+    //        set => SetValue(ImageSourceProperty, value);
+    //    }
+    //    public static readonly DependencyProperty ImageSourceProperty =
+    //        DependencyProperty.Register(
+    //            nameof(ImageSource),
+    //            typeof(string),
+    //            typeof(XLConfigTab2), // Use XLConfigTab2 as the owner, not PilotButton
+    //            new PropertyMetadata("/XLPilot;component/Resources/Images/default-profile-picture.png"));
+
+    //    public string ButtonText
+    //    {
+    //        get => (string)GetValue(ButtonTextProperty);
+    //        set => SetValue(ButtonTextProperty, value);
+    //    }
+    //    public static readonly DependencyProperty ButtonTextProperty =
+    //        DependencyProperty.Register(
+    //            nameof(ButtonText),
+    //            typeof(string),
+    //            typeof(XLConfigTab2), // Use XLConfigTab2 as the owner, not PilotButton
+    //            new PropertyMetadata(string.Empty));
+    }
+
+
+
+}
---
+++ UserControls/PilotButton.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace XLPilot.UserControls
+{
+    public partial class PilotButton : UserControl
+    {
+        public PilotButton()
+        {
+            InitializeComponent();
+        }
+
+
+        // ImageSource Dependency Property (keep the existing implementation)
+        public string ImageSource
+        {
+            get => (string)GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register(
+                nameof(ImageSource),
+                typeof(string),
+                typeof(PilotButton),
+                new PropertyMetadata("/XLPilot;component/Resources/Images/detault-profile-picture.png"));
+
+        // ButtonText Dependency Property (keep the existing implementation)
+        public string ButtonText
+        {
+            get => (string)GetValue(ButtonTextProperty);
+            set => SetValue(ButtonTextProperty, value);
+        }
+        public static readonly DependencyProperty ButtonTextProperty =
+            DependencyProperty.Register(
+                nameof(ButtonText),
+                typeof(string),
+                typeof(PilotButton),
+                new PropertyMetadata(string.Empty));
+    }
+
+}

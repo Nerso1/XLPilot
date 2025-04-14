9cf591ea9b75bfb8fcbbc766b3f1460ea0143548
Domin <dominiknowekonto@gmail.com>
2025-04-14 20:54:25 +0200

    Added ToolTip functionality to PilotButton, and implemented ToolTip showing conditionally, only when value in ToolTipText is provided and it's not empty

---
+++ UserControls/StringEmptyConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace XLPilot.UserControls
+{
+    public class StringEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Returns true if string is null or empty
+            return string.IsNullOrEmpty(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
